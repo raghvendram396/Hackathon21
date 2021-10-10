874640ec287928fa1fd1cf19304a7d07c362ed4a
Vaibhav Pandey <vp789792@gmail.com>
2021-10-10 12:41:45 +0530

    final stage started

--- app.js
+++ app.js
@@ -1,87 +1,127 @@
-
-require ('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+var FormData = require('form-data');
+var form_data = new FormData();
 const ejs = require("ejs");
-const mongoose=require("mongoose");
-const md5=require("md5");
-const app=express();
-app.use(express.urlencoded({extended: true}));
+const mongoose = require("mongoose");
+const md5 = require("md5");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-const user=process.env.MONGOUSER
-const pass=process.env.MONGOPASS
-mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.zksak.mongodb.net/Job_Portal?retryWrites=true&w=majority`,{useNewUrlParser: true,useUnifiedTopology:true});
-var flag=0;
+const user = process.env.MONGOUSER
+const pass = process.env.MONGOPASS
+mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.zksak.mongodb.net/Job_Portal?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+const fileUpload = require('express-fileupload'); // file upload
+app.use(fileUpload());
+var cloudinary = require("cloudinary").v2
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
+var flag = 0;
 var useridvar;
-const companySchema={
-	cin_number: String,
-	password: String,
-  company: String,
-  jobtitle: String,
-  WorkExp: String,
-  location: String,
-  post: String,
-  salary: String,
-  description: String
-};
-const userSchema={
-  email: String,
-  password: String
-};
-const trackSchema={
-  user_id: String,
-  company_id: String,
-  name: String,
-  contact: Number,
-  dob: Date,
-  IntermediateSname: String,
-  IntermediatePerc: String,
-  HighSname: String,
-  HighPerc: String,
-  workExp :String,
-  awards: String
+const Company_Profile_Schema = {
+    logo_url: String,
+    cin_number: String,
+    email: String,
+    password: String,
+    Company_name: String,
+    description: String,
+    contact_no: String,
+    ceo_name: String,
+    headquarter_location: String,
+    postal: Number
 }
-const CompanyJob=mongoose.model("CompanyJob",companySchema);
-const User=mongoose.model("User",userSchema);
-const Track=mongoose.model("Track",trackSchema);
+const companySchema = {
+    cin_number: String,
+    password: String,
+    company: String,
+    jobtitle: String,
+    WorkExp: String,
+    location: String,
+    post: String,
+    salary: String,
+    description: String
+};
+const userSchema = {
+    email: String,
+    password: String
+};
+const trackSchema = {
+    user_id: String,
+    company_id: String,
+    name: String,
+    contact: Number,
+    dob: Date,
+    IntermediateSname: String,
+    IntermediatePerc: String,
+    HighSname: String,
+    HighPerc: String,
+    workExp: String,
+    awards: String
+}
+const CompanyProfile = mongoose.model("CompanyProfile", Company_Profile_Schema);
+const CompanyJob = mongoose.model("CompanyJob", companySchema);
+const User = mongoose.model("User", userSchema);
+const Track = mongoose.model("Track", trackSchema);
 
 
-app.get("/",function(req,res)
-{if(flag===0)
-res.render("home",{quote:null,message:null});
-else
-{res.render("home",{quote:null,message: "Jobs Posted successfully!"});
-flag=0;
-}
+app.get("/", function(req, res) {
+    if (flag === 0)
+        res.render("home", { quote: null, message: null });
+    else {
+        res.render("home", { quote: null, message: "Jobs Posted successfully!" });
+        flag = 0;
+    }
 });
 
 
 
-app.post("/hiring",function(req,res)
-{res.render("hiring-page",{msg:null});});
+app.post("/hiring", function(req, res) { res.render("hiring-page", { msg: null }); });
 
-app.get("/company_registration",function(req,res)
-{res.render("New_Posting_Registration",{msg:null});});
+app.get("/company_login", function(req, res) { res.render("Company_login", { msg: 0 }); });
 
-app.get("/company_login",function(req,res)
-{res.render("Company_login",{msg:null});});
-
-app.post("/login_details_check",function(req,res){
-  CompanyJob.findOne({cin_number:req.body.cin_num,password:req.body.pass},(err,result)=>{
-    if(err){
-       res.send(err)
-    }
-    else{
-      res.send(result)
-    }
-  })
+app.post("/login_details_check", function(req, res) {
+    CompanyProfile.findOne({ cin_number: req.body.cin_num, email: req.body.email, password: req.body.pass }, (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            if (result === null) {
+                res.render("Company_login", { msg: 1 })
+            } else {
+                res.render("Company_Profile", { data: result })
+            }
+        }
+    })
 })
 
-app.post("/registrationform",function(req,res){
-	const company_name = req.body.company;
-	// CompanyJob.find({cin_number: req.body.cin},function(err,temp){
-			const value = new CompanyJob({
+app.get("/company_registration", function(req, res) { res.render("Company_Registration", { msg: null }); });
+
+app.post("/company_registartion_datails", (req, res) => {
+    CompanyProfile.findOne({ cin_number: req.body.cin_num, email: req.body.email }, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            if (result !== null) {
+                res.render("Company_Registration", { msg: "This CIN Number or Email has already taken" });
+            } else {
+                console.log(req)
+            }
+        }
+    })
+})
+
+app.post("/Check_Profile", (req, res) => {
+    res.render("Company_Profile", { data: req.body })
+})
+
+app.post("/registrationform", function(req, res) {
+    const company_name = req.body.company;
+    // CompanyJob.find({cin_number: req.body.cin},function(err,temp){
+    const value = new CompanyJob({
         company: req.body.company,
         cin_number: req.body.cin,
         password: req.body.pass,
@@ -90,138 +130,127 @@ app.post("/registrationform",function(req,res){
         location: req.body.location,
         post: req.body.post,
         salary: req.body.salary,
-        description:req.body.description
-	});
-	value.save(function(err){
-    if(err)
-    return console.log(err);
-    else{
-		res.render("home",{message:null,quote:"Posted Successfully!!"});
-    }
-	});
-	// });
+        description: req.body.description
+    });
+    value.save(function(err) {
+        if (err)
+            return console.log(err);
+        else {
+            res.render("home", { message: null, quote: "Posted Successfully!!" });
+        }
+    });
+    // });
 
 });
 
 
 
-app.post("/company",function(req,res)
-{const jobpost=new CompanyJob(
-  { company:req.body.company,
-    jobtitle:req.body.jobtitle,
-    workExp:req.body.wexperience,
-    location:req.body.location,
-    post:req.body.post,
-    salary:req.body.salary,
-   description:req.body.description
- }
-);
-jobpost.save(function()
-{flag=1;
-res.redirect("/");
-});
+app.post("/company", function(req, res) {
+    const jobpost = new CompanyJob({
+        company: req.body.company,
+        jobtitle: req.body.jobtitle,
+        workExp: req.body.wexperience,
+        location: req.body.location,
+        post: req.body.post,
+        salary: req.body.salary,
+        description: req.body.description
+    });
+    jobpost.save(function() {
+        flag = 1;
+        res.redirect("/");
+    });
 });
 
 
-app.get("/login",function(req,res)
-{
-CompanyJob.find({},function(err,list)
-{if(err)
-console.log(err);
-else
-{
-if(list)
-res.render("companylist",{jobs: list,userid: useridvar});
-}
-});});
-app.post("/apply",function(req,res)
-{
-Track.findOne({user_id: req.body.userid, company_id: req.body.id},function(err,found)
-{if(err)
-console.log(err);
-else {
-  if(found!=null)
-  {res.render("Already");}
-  else res.render("job",{id: req.body.id,userid: req.body.userid});
-}});
+app.get("/login", function(req, res) {
+    CompanyJob.find({}, function(err, list) {
+        if (err)
+            console.log(err);
+        else {
+            if (list)
+                res.render("companylist", { jobs: list, userid: useridvar });
+        }
+    });
 });
-app.post("/postajob",function(req,res)
-{ useridvar=req.body.userid;
-const jobid=req.body.job;
-const userid=req.body.userid;
-const trac=new Track({
-  user_id: userid,
-  company_id: jobid,
-  name: req.body.name,
-  dob: req.body.dob,
-  contact: req.body.contact,
-  IntermediateSname: req.body.school,
-  IntermediatePerc: req.body.perc,
-  HighSname: req.body.hschool,
-  HighPerc: req.body.hperc,
-  workExp: req.body.we,
-  awards: req.body.ah
+app.post("/apply", function(req, res) {
+    Track.findOne({ user_id: req.body.userid, company_id: req.body.id }, function(err, found) {
+        if (err)
+            console.log(err);
+        else {
+            if (found != null) { res.render("Already"); } else res.render("job", { id: req.body.id, userid: req.body.userid });
+        }
+    });
 });
-trac.save(function(err)
-{if(err)
-console.log(err);
-else
-res.redirect("/login");
+app.post("/postajob", function(req, res) {
+    useridvar = req.body.userid;
+    const jobid = req.body.job;
+    const userid = req.body.userid;
+    const trac = new Track({
+        user_id: userid,
+        company_id: jobid,
+        name: req.body.name,
+        dob: req.body.dob,
+        contact: req.body.contact,
+        IntermediateSname: req.body.school,
+        IntermediatePerc: req.body.perc,
+        HighSname: req.body.hschool,
+        HighPerc: req.body.hperc,
+        workExp: req.body.we,
+        awards: req.body.ah
+    });
+    trac.save(function(err) {
+        if (err)
+            console.log(err);
+        else
+            res.redirect("/login");
+    });
 });
+app.get("/register", function(req, res) { res.render("register", { imp: null }); });
+app.post("/register", function(req, res) {
+    User.find({ email: req.body.email }, function(err, userlist) {
+        if (err)
+            console.log(err);
+        else {
+            if (userlist.length === 0) {
+                const user = new User({
+                    email: req.body.email,
+                    password: md5(req.body.password)
+                });
+                user.save(function(err) {
+                    if (err)
+                        console.log(err);
+                    else
+                        res.render("registersucces");
+                });
+            } else {
+                res.render("register", { imp: "Email already Registered!" });
+            }
+        }
+    });
 });
-app.get("/register",function(req,res)
-{res.render("register",{imp:null});});
-app.post("/register",function(req,res)
-{
-User.find({email: req.body.email},function(err,userlist)
-{if(err)
-console.log(err);
-else
-{
-if(userlist.length===0)
-{const user=new User({
-email: req.body.email,
-password: md5(req.body.password)
-});
-user.save(function(err)
-{if(err)
-  console.log(err);
-else
-res.render("registersucces");
-}
-);
-}
-else {
-  res.render("register",{imp: "Email already Registered!"});
-}
-}
-});
-});
-app.post("/login",function(req,res)
-{ const email=req.body.email;
-const password=md5(req.body.password);
-User.findOne({email: email},function(err,founduser)
-{if(err)
-console.log(err);
-else{
-  if(founduser)
-  {if(founduser.password===password)
-  {CompanyJob.find({},function(err,list)
-  {if(err)
-  console.log(err);
-  else
-  {
-  if(list)
-  res.render("companylist",{jobs: list,userid: founduser._id});
-  }
-  });}
-  else {
-    res.render("home",{quote: "Wrong Password",message:null});
-  }
+app.post("/login", function(req, res) {
+    const email = req.body.email;
+    const password = md5(req.body.password);
+    User.findOne({ email: email }, function(err, founduser) {
+        if (err)
+            console.log(err);
+        else {
+            if (founduser) {
+                if (founduser.password === password) {
+                    CompanyJob.find({}, function(err, list) {
+                        if (err)
+                            console.log(err);
+                        else {
+                            if (list)
+                                res.render("companylist", { jobs: list, userid: founduser._id });
+                        }
+                    });
+                } else {
+                    res.render("home", { quote: "Wrong Password", message: null });
+                }
 
-  }
-  else res.render("home",{quote: "Invalid email and password",message:null});
-}
-})});
-app.listen(process.env.PORT || 3000,function()
-{console.log("Server successfully ran!");});
+            } else res.render("home", { quote: "Invalid email and password", message: null });
+        }
+    })
+});
+app.listen(process.env.PORT || 3000, function() { console.log("Server successfully ran!"); });
---
+++ public/js/gsdk-bootstrap-wizard.js
@@ -0,0 +1,280 @@
+searchVisible = 0;
+transparent = true;
+
+$(document).ready(function() {
+
+    /*  Activate the tooltips      */
+    $('[rel="tooltip"]').tooltip();
+
+    // Code for the Validator
+    var $validator = $('.wizard-card form').validate({
+        rules: {
+            c_name: {
+                required: true,
+                minlength: 3
+            },
+            cin_num: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                minlength: 3,
+            },
+            pass: {
+                required: true,
+                minlength: 6,
+            },
+            repass: {
+                required: true,
+                equalTo: "#paw"
+            },
+            ceo_name: {
+                required: true,
+                minlength: 2,
+            },
+            phone:{
+                maxlength: 11
+            },
+            postal:{
+                maxlength:6
+            }
+        }
+    });
+    // ---------------------Password strength-------------------------
+    var pw_test = function(str) {
+        var WeakPass = /(?=.{5,}).*/; //Must contain 5 characters or more
+        var MediumPass = /^(?=\S*?[a-z])(?=\S*?[A-Z])\S{5,}$/; //Must contain lower case letters and at least one digit.
+        var StrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])\S{5,}$/; //Must contain at least one upper case letter, one lower case letter and one digit.
+        var VryStrongPass = /^(?=\S*?[A-Z])(?=.*[@$!%*#?&])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[^\w\*])\S{5,}$/; //Must contain at least one upper case letter, one lower case letter and one digit.
+
+        if (VryStrongPass.test(str)) {
+            return {
+                strength: 'very_strong',
+                text: 'Very Strong! (Awesome, please don\'t forget your pass now!)'
+            };
+        } else if (StrongPass.test(str)) {
+            return {
+                strength: 'strong',
+                text: 'Strong! (Enter Special Characters to make even stronger)'
+            };
+        } else if (MediumPass.test(str)) {
+            return {
+                strength: 'medium',
+                text: 'Good! (Enter Number letter to make strong)'
+            };
+        } else if (WeakPass.test(str)) {
+            return {
+                strength: 'weak',
+                text: 'Still Weak! (Enter Uppercase and Lowercase to make good password)'
+            };
+        } else {
+            return {
+                strength: 'very_weak',
+                text: 'Very Weak! (Must be 5 or more chars)'
+            };
+        }
+    };
+    var password1 = $('#paw'); //id of first password field
+    var strength_indicator = $('#strength-indicator'); //id of indicator element
+
+    //check password strength when user types in
+    $(password1).on('keyup', function(e) {
+        var pwd_strength = pw_test(password1.val());
+        switch (pwd_strength.strength) {
+            case 'very_weak':
+                strength_indicator.removeClass().addClass('very_weak').html(pwd_strength.text).css("color","#f20a0a");
+                break;
+            case 'weak':
+                strength_indicator.removeClass().addClass('weak').html(pwd_strength.text).css("color","#f50535");
+                break;
+            case 'medium':
+                strength_indicator.removeClass().addClass('medium').html(pwd_strength.text).css("color","#f58905");
+                break;
+            case 'strong':
+                strength_indicator.removeClass().addClass('strong').html(pwd_strength.text).css("color","#6ECB63");
+                break;
+            case 'very_strong':
+                strength_indicator.removeClass().addClass('very_strong').html(pwd_strength.text).css("color","#3d9c14");
+                break;
+        }
+    });
+    //----------------------------------end---------------------------------------------
+
+    // ---------------------------------Logo Upload-------------------------------------
+    
+    $("#wizard-picture").change(function(e){
+        if(e.target.files[0].type=="image/jpeg" || e.target.files[0].type=="image/png"){
+            readURL(this);
+        }
+    })
+
+    // Wizard Initialization
+    $('.wizard-card').bootstrapWizard({
+        'tabClass': 'nav nav-pills',
+        'nextSelector': '.btn-next',
+        'previousSelector': '.btn-previous',
+
+        onNext: function(tab, navigation, index) {
+            var $valid = $('.wizard-card form').valid();
+            if (!$valid) {
+                $validator.focusInvalid();
+                return false;
+            }
+        },
+
+        onInit: function(tab, navigation, index) {
+
+            //check number of tabs and fill the entire row
+            var $total = navigation.find('li').length;
+            $width = 100 / $total;
+            var $wizard = navigation.closest('.wizard-card');
+
+            $display_width = $(document).width();
+
+            if ($display_width < 600 && $total > 3) {
+                $width = 50;
+            }
+
+            navigation.find('li').css('width', $width + '%');
+            $first_li = navigation.find('li:first-child a').html();
+            $moving_div = $('<div class="moving-tab">' + $first_li + '</div>');
+            $('.wizard-card .wizard-navigation').append($moving_div);
+            refreshAnimation($wizard, index);
+            $('.moving-tab').css('transition', 'transform 0s');
+        },
+
+        onTabClick: function(tab, navigation, index) {
+
+            var $valid = $('.wizard-card form').valid();
+
+            if (!$valid) {
+                return false;
+            } else {
+                return true;
+            }
+        },
+
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index + 1;
+
+            var $wizard = navigation.closest('.wizard-card');
+
+            // If it's the last tab then hide the last button and show the finish instead
+            if ($current >= $total) {
+                $($wizard).find('.btn-next').hide();
+                $($wizard).find('.btn-finish').show();
+            } else {
+                $($wizard).find('.btn-next').show();
+                $($wizard).find('.btn-finish').hide();
+            }
+
+            button_text = navigation.find('li:nth-child(' + $current + ') a').html();
+
+            setTimeout(function() {
+                $('.moving-tab').text(button_text);
+            }, 150);
+
+            var checkbox = $('.footer-checkbox');
+
+            if (!index == 0) {
+                $(checkbox).css({
+                    'opacity': '0',
+                    'visibility': 'hidden',
+                    'position': 'absolute'
+                });
+            } else {
+                $(checkbox).css({
+                    'opacity': '1',
+                    'visibility': 'visible'
+                });
+            }
+
+            refreshAnimation($wizard, index);
+        }
+    });
+
+
+    // Prepare the preview for profile picture
+    // $("#wizard-picture").change(function() {
+    //     readURL(this);
+    //     console.log(readURL(this))
+    // });
+
+    $('[data-toggle="wizard-radio"]').click(function() {
+        wizard = $(this).closest('.wizard-card');
+        wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
+        $(this).addClass('active');
+        $(wizard).find('[type="radio"]').removeAttr('checked');
+        $(this).find('[type="radio"]').attr('checked', 'true');
+    });
+
+    $('[data-toggle="wizard-checkbox"]').click(function() {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).find('[type="checkbox"]').removeAttr('checked');
+        } else {
+            $(this).addClass('active');
+            $(this).find('[type="checkbox"]').attr('checked', 'true');
+        }
+    });
+
+    $('.set-full-height').css('height', 'auto');
+
+});
+
+
+
+//Function to show image before upload
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(window).resize(function() {
+    $('.wizard-card').each(function() {
+        $wizard = $(this);
+        index = $wizard.bootstrapWizard('currentIndex');
+        refreshAnimation($wizard, index);
+
+        $('.moving-tab').css({
+            'transition': 'transform 0s'
+        });
+    });
+});
+
+function refreshAnimation($wizard, index) {
+    total_steps = $wizard.find('li').length;
+    move_distance = $wizard.width() / total_steps;
+    step_width = move_distance;
+    move_distance *= index;
+
+    $wizard.find('.moving-tab').css('width', step_width);
+    $('.moving-tab').css({
+        'transform': 'translate3d(' + move_distance + 'px, 0, 0)',
+        'transition': 'all 0.3s ease-out'
+
+    });
+}
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    };
+};
